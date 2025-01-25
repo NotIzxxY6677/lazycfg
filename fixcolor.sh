e10d5af0dc07a2c6a52a4d80d45e5af912000b36
NotIzxxY6677 <81343518+NotIzxxY6677@users.noreply.github.com>
2025-01-26 00:56:25 +0530

    Update fixcolor.sh

--- fixcolor.sh
+++ fixcolor.sh
@@ -1,3 +1,16 @@
-!/bin/bash
+#!/bin/bash
 
-sudo bash -c 'echo -e "#!/bin/bash\n\nreadarray -t proptest_result <<<\"\$(/usr/bin/proptest -M i915 -D /dev/dri/card0 | grep -E \'Broadcast|Connector\')\"\n\nfor ((i = 0; i < \${#proptest_result[*]}; i += 2)); do\n    connector=\$(echo \${proptest_result[i]} | awk \x27{print \$2}\x27)\n    connector_id=\$(echo \${proptest_result[i + 1]} | awk \x27{print \$1}\x27)\n\n    /usr/bin/proptest -M i915 \$connector connector \$connector_id 1\ndone" > /usr/local/bin/intel-wayland-fix-full-color'
+sudo bash -c 'cat > /usr/local/bin/intel-wayland-fix-full-color <<EOF
+#!/bin/bash
+
+readarray -t proptest_result <<<"\$(/usr/bin/proptest -M i915 -D /dev/dri/card0 | grep -E \'Broadcast|Connector\')"
+
+for ((i = 0; i < \${#proptest_result[*]}; i += 2)); do
+    connector=\$(echo \${proptest_result[i]} | awk \x27{print \$2}\x27)
+    connector_id=\$(echo \${proptest_result[i + 1]} | awk \x27{print \$1}\x27)
+
+    /usr/bin/proptest -M i915 \$connector connector \$connector_id 1
+done
+EOF'
+
+sudo chmod +x /usr/local/bin/intel-wayland-fix-full-color
