b7d05214e520f0453a2d5ff23e48006f84488bf2
NotIzxxY6677 <81343518+NotIzxxY6677@users.noreply.github.com>
2025-01-29 17:23:01 +0530

    Update fix_colors_for_wayland_intel.sh

--- fix_colors_for_wayland_intel.sh
+++ fix_colors_for_wayland_intel.sh
@@ -1,2 +1,6 @@
-sudo cp /etc/udev/rules.d/
-sudo cp /usr/local/bin/
+#!/bin/bash
+
+sudo cp 80-i915.rules /etc/udev/rules.d/
+sudo cp intel-wayland-fix-full-color /usr/local/bin/
+
+# Add one more unit to your native screen's refresh rate if this does not work.
