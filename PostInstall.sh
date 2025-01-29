4ed072bee3cebff8b35f528169b100de0a109064
NotIzxxY6677 <81343518+NotIzxxY6677@users.noreply.github.com>
2025-01-29 17:18:42 +0530

    Update PostInstall.sh

--- PostInstall.sh
+++ PostInstall.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-#Create a Normal User account MANUALLY.
-pacman -S sudo nano
-useradd -m -G wheel -s /bin/bash pc
+#Create a Normal User account MANUALLY and get sudo installed.
+#pacman -S sudo nano
+#useradd -m -G wheel -s /bin/bash pc
 #passwd pc
 #<PASSWORD>
-#Uncomment wheel from sudoers file using nano in '/etc/sudoers' .
+#Uncomment wheel from sudoers file using nano(or your preferred text editor in CLI) in '/etc/sudoers' .
 
 #Install driver-related packages:
 sudo pacman -S mesa vulkan-intel intel-media-driver libva-intel-driver libva-utils intel-gpu-tools
 
 #Install libraries-type packages?
 sudo pacman -S opencl-headers clinfo ocl-icd
-
-#Install printer related drivers/apps:
-sudo pacman -S hplip
 
 #Install yay (AUR Helper)
 sudo pacman -S --needed base-devel git
@@ -24,6 +21,6 @@ makepkg -si
 yay --version
 
 #Install hyprland ecosystem stuff, for packages that don't exist in official repo, just use YAY
-#sudo pacman -S hyprpaper hyprpicker hypridle hyprlock xdg-desktop-portal-hyprland hyprsysteminfo hyprsunset hyprpolkitagent hyprland-qt-support qt6-wayland qt5-wayland
+#sudo pacman -S hyprpaper hyprpicker hypridle hyprlock xdg-desktop-portal-hyprland hyprpolkitagent hyprland-qt-support qt6-wayland qt5-wayland
 
-# READ https://wiki.archlinux.org/title/Intel_graphics#Washed_out_colors
+# "Read https://wiki.archlinux.org/title/Intel_graphics#Washed_out_colors. By the way, another potential fix for washed-out colors is to set your refresh rate to one unit higher than your screen's native refresh rate. This has worked for me."
