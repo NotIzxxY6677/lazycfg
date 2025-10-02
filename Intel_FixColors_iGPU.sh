2515a8357e61c9d63904fbb90ee34a6953d8c54c
NotIzxxY6677 <81343518+NotIzxxY6677@users.noreply.github.com>
2025-10-03 02:55:39 +0530

    Update Intel_FixColors_iGPU.sh

--- Intel_FixColors_iGPU.sh
+++ Intel_FixColors_iGPU.sh
@@ -4,3 +4,4 @@ sudo cp 80-i915.rules /etc/udev/rules.d/
 sudo cp intel-wayland-fix-full-color /usr/local/bin/
 
 # Add one more unit to your native screen refresh rate if this does not work.
+# https://wiki.archlinux.org/title/Intel_graphics#Washed_out_colors
