5840d7ac7723ff328c55de1aba0c60bfbb698b29
NotIzxxY6677 <81343518+NotIzxxY6677@users.noreply.github.com>
2025-10-03 02:42:54 +0530

    Update Intel_FixColors_iGPU.sh

--- Intel_FixColors_iGPU.sh
+++ Intel_FixColors_iGPU.sh
@@ -3,4 +3,4 @@
 sudo cp 80-i915.rules /etc/udev/rules.d/
 sudo cp intel-wayland-fix-full-color /usr/local/bin/
 
-# Add one more unit to your native screen's refresh rate if this does not work.
+# Add one more unit to your native screen refresh rate if this does not work.
